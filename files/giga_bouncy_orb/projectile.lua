5cafb530c781aa6b26615ff69e21c200246be4f0
Fasteroid <29342750+Fasteroid@users.noreply.github.com>
2026-03-12 19:26:24 -0400

    spell icon + numbers tweaks

--- files/giga_bouncy_orb/projectile.lua
+++ files/giga_bouncy_orb/projectile.lua
@@ -2,8 +2,8 @@ dofile_once( "data/scripts/lib/utilities.lua" )
 
 local entity_id    = GetUpdatedEntityID()
 
-local BEAM_LENGTH = 10
-local BEAM_WIDTH  = 2
+local BEAM_LENGTH = 8
+local BEAM_WIDTH  = 3
 local BEAM_COUNT  = 12
 
 local function AddLaserEmitter(entity_id, angle)
@@ -12,13 +12,13 @@ local function AddLaserEmitter(entity_id, angle)
         laser_angle_add_rad = angle,
     })
 
-    ComponentObjectSetValue2(laser_comp, "laser", "damage_to_entities",                    0.12           )
+    ComponentObjectSetValue2(laser_comp, "laser", "damage_to_entities",                    0.03           )
     ComponentObjectSetValue2(laser_comp, "laser", "max_cell_durability_to_destroy",        14             )
-    ComponentObjectSetValue2(laser_comp, "laser", "damage_to_cells",                       500            )
+    ComponentObjectSetValue2(laser_comp, "laser", "damage_to_cells",                       1500           )
     ComponentObjectSetValue2(laser_comp, "laser", "root_entity_is_responsible_for_damage", true           )
     ComponentObjectSetValue2(laser_comp, "laser", "max_length",                            BEAM_LENGTH    )
     ComponentObjectSetValue2(laser_comp, "laser", "beam_radius",                           BEAM_WIDTH     )
-    ComponentObjectSetValue2(laser_comp, "laser", "hit_particle_chance",                   20             )
+    ComponentObjectSetValue2(laser_comp, "laser", "hit_particle_chance",                   40             )
     ComponentObjectSetValue2(laser_comp, "laser", "beam_particle_chance",                  95             )
     ComponentObjectSetValue2(laser_comp, "laser", "beam_particle_type",                    3              )
 end
--- files/giga_bouncy_orb/spell.lua
+++ files/giga_bouncy_orb/spell.lua
@@ -2,11 +2,11 @@ table.insert( actions, {
 	id          = "GIGA_BOUNCY_ORB",
 	name 		= "$action_giga_bouncy_orb",
 	description = "$actiondesc_giga_bouncy_orb",
-	sprite 		= "data/ui_gfx/gun_actions/bouncy_orb.png",
+	sprite 		= "mods/more_giga_spells/files/giga_bouncy_orb/spell.png",
 	related_projectiles	= {"mods/more_giga_spells/files/giga_bouncy_orb/projectile.xml"},
 	type 		= ACTION_TYPE_PROJECTILE,
-	spawn_level                       = "0,2,4", -- BOUNCY_ORB
-	spawn_probability                 = "1,1,1", -- BOUNCY_ORB
+	spawn_level                       = "0,2,4", -- DISC_BULLET_BIG
+	spawn_probability                 = "0.6,0.7,0.8", -- DISC_BULLET_BIG
 	price = 180,
 	mana = 38,
 	--max_uses = 40,
@@ -15,6 +15,7 @@ table.insert( actions, {
 		-- damage = 0.3
         c.fire_rate_wait = c.fire_rate_wait + 40
         c.spread_degrees = c.spread_degrees + 6
-        shot_effects.recoil_knockback = shot_effects.recoil_knockback + 160.0
+		c.game_effect_entities = c.game_effect_entities .. "data/entities/misc/effect_disintegrated.xml,"
+        shot_effects.recoil_knockback = shot_effects.recoil_knockback + 140.0
 	end,
 })
