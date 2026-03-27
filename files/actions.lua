2882b2b59689d3f259f61429b47e0493768a26b2
Fasteroid <29342750+Fasteroid@users.noreply.github.com>
2026-03-27 19:00:27 -0400

    rename

--- files/actions.lua
+++ files/actions.lua
@@ -1,1 +1,1 @@
-dofile_once("mods/more_giga_spells/files/giga_bouncy_orb/spell.lua")
+dofile_once("mods/more_giga_spells/files/giga_energy_sphere/spell.lua")
--- files/giga_energy_sphere/spell.lua
+++ files/giga_energy_sphere/spell.lua
@@ -1,9 +1,9 @@
 table.insert( actions, {
-	id          = "GIGA_BOUNCY_ORB",
-	name 		= "$action_giga_bouncy_orb",
-	description = "$actiondesc_giga_bouncy_orb",
-	sprite 		= "mods/more_giga_spells/files/giga_bouncy_orb/spell.png",
-	related_projectiles	= {"mods/more_giga_spells/files/giga_bouncy_orb/projectile.xml"},
+	id          = "GIGA_ENERGY_SPHERE",
+	name 		= "$action_giga_energy_sphere",
+	description = "$actiondesc_giga_energy_sphere",
+	sprite 		= "mods/more_giga_spells/files/giga_energy_sphere/spell.png",
+	related_projectiles	= {"mods/more_giga_spells/files/giga_energy_sphere/projectile.xml"},
 	type 		= ACTION_TYPE_PROJECTILE,
 	spawn_level                       = "0,2,4", -- DISC_BULLET_BIG
 	spawn_probability                 = "0.6,0.7,0.8", -- DISC_BULLET_BIG
@@ -11,7 +11,7 @@ table.insert( actions, {
 	mana = 38,
 	--max_uses = 40,
 	action 		= function()
-		add_projectile("mods/more_giga_spells/files/giga_bouncy_orb/projectile.xml")
+		add_projectile("mods/more_giga_spells/files/giga_energy_sphere/projectile.xml")
 		-- damage = 0.3
         c.fire_rate_wait = c.fire_rate_wait + 40
         c.spread_degrees = c.spread_degrees + 6
