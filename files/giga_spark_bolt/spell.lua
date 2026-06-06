fcd4177e8eedbc5e5fdbc1e6bafba2d6fe06dd41
Fasteroid <fasteroid@hotmail.com>
2026-06-05 21:47:10 -0400

    made giga spark a *tad* more dangerous

--- files/giga_spark_bolt/spell.lua
+++ files/giga_spark_bolt/spell.lua
@@ -8,7 +8,7 @@ table.insert( actions, {
 	spawn_level                       = "0,2,4", -- DISC_BULLET_BIG
 	spawn_probability                 = "0.6,0.6,0.7", -- DISC_BULLET_BIG
 	price = 150,
-	mana = 17,
+	mana = 21,
 	--max_uses = 40,
 	action 		= function()
 		add_projectile("mods/more_giga_spells/files/giga_spark_bolt/projectile.xml")
@@ -16,7 +16,7 @@ table.insert( actions, {
         c.fire_rate_wait = c.fire_rate_wait + 20
         c.spread_degrees = c.spread_degrees + 3
 		c.screenshake    = c.screenshake    + 2
-		c.damage_critical_chance = c.damage_critical_chance + 50
+		c.damage_critical_chance = c.damage_critical_chance + 100
         shot_effects.recoil_knockback = shot_effects.recoil_knockback + 80.0
 	end,
 })
