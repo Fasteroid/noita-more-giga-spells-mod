8f4ac4886ece1169d00fe82a2a17f5f948399f92
Fasteroid <fasteroid@hotmail.com>
2026-07-16 19:37:54 -0400

    make giga spitter funnier

--- files/giga_spitter_bolt/projectile.lua
+++ files/giga_spitter_bolt/projectile.lua
@@ -6,26 +6,8 @@ local pos_x, pos_y = EntityGetTransform( projectile )
 local velComponent = EntityGetFirstComponent( projectile, "VelocityComponent" )
 local vx, vy = ComponentGetValue2( velComponent, "mVelocity")
 
-local how_many = 5
+-- appear somewhere "behind" where it spawned
+local theta = math.atan2(-vy, -vx) + (math.random() - 0.5) * 3.1416926 * 0.5;
+local length = math.random() * 50 + 20
 
-for i=1, how_many do
-
-    local theta = math.random() * 2 * 3.1416926
-    local length = math.random() * 250
-
-    local spark = EntityLoad( "data/entities/projectiles/deck/spitter.xml", pos_x, pos_y)
-
-    local vel_x = math.cos( theta ) * length
-    local vel_y = math.sin( theta ) * length
-
-    edit_component( spark, "ProjectileComponent", function(comp,vars)
-        ComponentSetValue2( comp, "friendly_fire", true )
-        ComponentSetValue2( comp, "collide_with_shooter_frames", 0 )
-        ComponentSetValue2( comp, "mWhoShot", projectile )
-    end)
-
-    edit_component( spark, "VelocityComponent", function(comp,vars)
-        ComponentSetValueVector2( comp, "mVelocity", vel_x + vx, vel_y + vy)
-    end)
-
-end
+EntitySetTransform(projectile, pos_x + math.cos(theta) * length, pos_y + math.sin(theta) * length)
